e1e818e8acb15ea8aea086ac72769547235f791a
ThanhVu206 <thanhvu5112006@gmail.com>
2025-12-19 23:46:27 +0700

    update

---
+++ js/muahang.js
@@ -0,0 +1,8 @@
+function muaHang(img, name, price) {
+    localStorage.setItem("img", img);
+    localStorage.setItem("name", name);
+    localStorage.setItem("price", price);
+
+    // LUÔN ĐÚNG cho mọi trang trong /html
+    window.location.href = "../html/thanhtoan.html";
+}
